08b6988b9cded4820bbff47a8b4686f5607d5758
Satoshi Nakamura <kamu@kamusoft-mini-2.local>
2019-08-16 19:16:28 +0900

    Fix Sample

--- Sample/Sample/ViewModels/DemoPageViewModel.cs
+++ Sample/Sample/ViewModels/DemoPageViewModel.cs
@@ -55,7 +55,7 @@ namespace Sample.ViewModels
             _pageDlg = pageDialog;
 
             var list1 = new List<PhotoItem>();
-            for (var i = 0; i < 20; i++)
+            for (var i = 0; i < 2; i++)
             {
                 list1.Add(new PhotoItem
                 {
@@ -65,7 +65,7 @@ namespace Sample.ViewModels
                 });
             }
             var list2 = new List<PhotoItem>();
-            for (var i = 10; i < 15; i++)
+            for (var i = 10; i < 13; i++)
             {
                 list2.Add(new PhotoItem
                 {
@@ -93,6 +93,10 @@ namespace Sample.ViewModels
             ItemsSource.Add(group3);
             ItemsSourceH.Add(group1);
             ItemsSourceH.Add(group2);
+
+            PortraitColumns.Value = 4;
+
+
 
             TapCommand.Subscribe(async item => {
                 var photo = item as PhotoItem;
